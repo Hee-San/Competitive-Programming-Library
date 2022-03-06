21a5cc442928f148a06933034f017c973aaf4106
t.hirashima <hira9603859504@gmail.com>
2022-03-06 16:47:46 +0900

    Update example.test.cpp

--- example.test.cpp
+++ example.test.cpp
@@ -1,23 +1,11 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B"
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#define REP(i, n) for (int i = 0; (i) < (int)(n); ++ (i))
-#define ALL(x) std::begin(x), std::end(x)
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main() {
-    int n; cin >> n;
-    vector<int> s(n);
-    REP (i, n) {
-        cin >> s[i];
-    }
-    int q; cin >> q;
-    int cnt = 0;
-    while (q --) {
-        int t_i; cin >> t_i;
-        cnt += binary_search(ALL(s), t_i);
-    }
-    cout << cnt << endl;
-    return 0;
+    ll A, B;
+    cin >> A >> B;
+    
+    ll ans = A + B;
+    cout << ans << endl;
 }
