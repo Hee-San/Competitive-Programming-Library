1cd1778e7d3baf36aea2cc4e65f5b86c0b165bd9
t.hirashima <hira9603859504@gmail.com>
2022-03-07 23:59:44 +0900

    Fix problem url

--- Tests/AizuOnlineJudge/DSL_2_B.test.cpp
+++ Tests/AizuOnlineJudge/DSL_2_B.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_A"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/problems/DSL_2_B"
 /**
  * @brief 区間の和・一点加算 Range Sum Query (RSQ)
  */
