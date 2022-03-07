ae5d795b1ec15cc26b18afbdb5e1ed2b790dba52
t.hirashima <hira9603859504@gmail.com>
2022-03-07 15:46:54 +0900

    Add RMQ

---
+++ Tests/LibraryChecker/Data_Structure/staticrmq.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+/**
+ * @brief Static RMQ
+ */
+
+#include <bits/stdc++.h>
+
+#include "../../structure/segment_tree/segment_tree.cpp"
+
+using namespace std;
+typedef long long ll;
+typedef vector<ll> vi;
+
+int main() {
+    ll N, Q;
+    cin >> N >> Q;
+
+    vi A(N);
+    for (ll i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+
+    SegmentTree<ll> seg(
+        N, [](ll a, ll b) { return min(a, b); }, LLONG_MAX);
+    seg.build(A);
+
+    for (ll i = 0; i < Q; i++) {
+        ll l, r;
+        cin >> l >> r;
+        cout << seg.get(l, r) << endl;
+    }
+}
