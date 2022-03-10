7db8882ab48a26a55983f8644542d1d801413945
t.hirashima <hira9603859504@gmail.com>
2022-03-11 00:24:12 +0900

    Fix bug

--- structure/segment_tree/segment_tree.cpp
+++ structure/segment_tree/segment_tree.cpp
@@ -39,8 +39,8 @@ struct SegmentTree {
 
     T get(int l, int r, T left = identity, T right = identity) {
         for (l += n, r += n; l < r; l /= 2, r /= 2) {
-            if (l % 2) ans = function(left, seg[l++]);
-            if (r % 2) ans = function(seg[--r], right);
+            if (l % 2) left = function(left, seg[l++]);
+            if (r % 2) right = function(seg[--r], right);
         }
         return function(left, right);
     }
